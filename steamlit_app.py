05182ce8b1f75cbd7077c68c0c124f9d5cf899ad
shraddha2504 <49411040+shraddha2504@users.noreply.github.com>
2023-02-06 13:10:36 +0530

    Update steamlit_app.py
    added breakfast menu header and 3items

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,3 +1,6 @@
 
 import streamlit
-streamlit.title('My parents New Healthy Dinner Breakfast Meanu \nomega3 & omega5')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
