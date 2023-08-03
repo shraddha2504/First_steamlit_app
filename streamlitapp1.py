58212680350d2f57efda72511d174a209598c2c8
shraddha2504 <49411040+shraddha2504@users.noreply.github.com>
2023-08-03 16:51:03 +0530

    Update streamlitapp1.py

--- streamlitapp1.py
+++ streamlitapp1.py
@@ -1,4 +1,6 @@
 import streamlit
-!pip install "snowflake-snowpark-python[pandas]"
-import pandas
+import snowflake.connector
 streamlit.header('Breakfast Menu')
+pip install snowflake-snowpark-python
+import pandas
+
